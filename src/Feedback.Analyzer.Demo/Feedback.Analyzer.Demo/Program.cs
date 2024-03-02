32d2728b13ee449045aa92d4f3d38aa88921fa66
Botirjon Abdumannonov <thedavlativich@gmail.com>
2024-03-02 16:35:28 +0500

    Initialize solution

---
+++ src/Feedback.Analyzer.Demo/Feedback.Analyzer.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿//create configurations
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddUserSecrets<Program>();
+var configuration = configurationBuilder.Build();
+
+//create kernel builder 
+var kernelBuilder = Kernel.CreateBuilder();
+
+//Add OpenAI connector
+kernelBuilder.AddOpenAIChatCompletion(modelId:"gpt-3.5-turbo", apiKey: configuration["OpenAiApiSettings:ApiKey"]!);
+
+//Build kernel
+var kernel = kernelBuilder.Build();
